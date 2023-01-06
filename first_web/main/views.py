3e05bb72172cfd6459edfc3ece34d6dd037e23ce
coder6497 <rty.sem@yandex.ru>
2023-01-07 00:03:22 +0300

    templates and css file

--- first_web/main/views.py
+++ first_web/main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>ДРИСТ</h1>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse("<h1>О сайте</h1>")
+    return render(request, 'main/about.html')
