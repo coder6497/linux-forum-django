008fe4192fa5e9c87d68e3f7cc060a35570f2f20
coder6497 <rty.sem@yandex.ru>
2023-01-10 22:52:13 +0300

    db and superuser

--- first_web/notes/models.py
+++ first_web/notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField("Название", max_length=80)
+    full_text = models.TextField("Тема")
+    date_time = models.DateTimeField("Дата публикации")
+    user_name = models.CharField("Пользователь", max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тема"
+        verbose_name_plural = 'Темы'
